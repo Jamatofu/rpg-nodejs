3088f562894e7c1ed6fa863b8ca99a990ac52086
Jamatofu <clement.beal96@gmail.com>
2018-02-07 14:28:57 +0100

    Server sends the client's page

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
-var app = require('express')();
-app.listen(8080);
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
 
-app.get('/', (req, res) => {
-  res.send('hello');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 8080);
+app.use(express.static('client'));
+
+app.get("/", function(req, res){
+    res.sendFile("index.html");
+});
+
+// Starts the server.
+server.listen(8080, function() {
+  console.log('Starting server on port 8080');
 });
