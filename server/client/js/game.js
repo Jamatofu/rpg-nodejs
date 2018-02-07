d36fe09bf8cad1ed1c6ae4090e9d11aef7c9275e
Jamatofu <clement.beal96@gmail.com>
2018-02-07 14:37:46 +0100

    Can detect arrow keys

--- server/client/js/game.js
+++ server/client/js/game.js
@@ -2,3 +2,20 @@ function logIn(id ) {
   console.log("Log : " + id);
   document.getElementById(id).style.display = "none";
 }
+
+window.onkeydown = function(event) {
+    switch (event.keyCode) {
+      case 37:
+          console.log("Left step");
+        break;
+      case 38:
+          console.log("Up step");
+        break;
+      case 39:
+          console.log("Right step");
+        break;
+      case 40:
+          console.log("Down step");
+        break;
+    }
+};
